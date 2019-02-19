472505a6cf55a558e5c6fb87e197004722753abf
flybirdxx <1119577418@qq.com>
2019-02-19 17:42:14 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/yangh/coolweather/db/County.java
@@ -0,0 +1,45 @@
+package com.yangh.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yangH on 2019/2/19.
+ */
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int weatherId;
+    private int ciytyId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCiytyId() {
+        return ciytyId;
+    }
+
+    public void setCiytyId(int ciytyId) {
+        this.ciytyId = ciytyId;
+    }
+}
